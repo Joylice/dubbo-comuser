82a475b3173374ee5f2b3eb0b7dd7a352440711b
wangmeng <11@qq.com>
2020-04-10 14:55:14 +0800

    1

--- src/main/java/com/monitor/comuser/ApiController.java
+++ src/main/java/com/monitor/comuser/ApiController.java
@@ -4,6 +4,7 @@ import com.alibaba.dubbo.config.annotation.Reference;
 
 
 import com.dubbo.common.api.MonitorServiceInterface;
+import com.dubbo.common.entity.HardwareProperties;
 import com.dubbo.common.entity.ServerProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,11 +16,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/pc")
 public class ApiController {
-    @Reference(check = false)
+    @Reference(version = "192.168.1.231", check = false, interfaceClass = MonitorServiceInterface.class)
     public MonitorServiceInterface monitorServiceInterface;
+
+    @Reference(version = "192.168.1.232", check = false, interfaceClass = MonitorServiceInterface.class)
+    public MonitorServiceInterface monitorServiceInterface02;
+
+    @Reference(version = "192.168.1.235", check = false, interfaceClass = MonitorServiceInterface.class)
+    public MonitorServiceInterface monitorServiceInterface03;
 
     @RequestMapping(value = "/status")
     public ServerProperties getStatus() {
         return monitorServiceInterface.getPCState();
     }
+
+    @RequestMapping(value = "/hand")
+    public HardwareProperties getHand() {
+        return monitorServiceInterface.getHardwarePro();
+    }
+
+    @RequestMapping(value = "/status02")
+    public ServerProperties getStatus02() {
+        return monitorServiceInterface02.getPCState();
+    }
+
+    @RequestMapping(value = "/status03")
+    public ServerProperties getStatus03() {
+        return monitorServiceInterface03.getPCState();
+    }
+
 }
--- src/main/java/com/monitor/comuser/WebApplication.java
+++ src/main/java/com/monitor/comuser/WebApplication.java
@@ -1,5 +1,6 @@
 package com.monitor.comuser;
 
+import com.alibaba.dubbo.config.annotation.Service;
 import com.alibaba.dubbo.config.spring.context.annotation.EnableDubbo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
